acdd13893cfafcecad0beac37f8bf88a40da3bbc
Abhinav Singh <abhinavs.forwork@gmail.com>
2026-02-07 17:07:14 +0530

    Implement Device List GUI with Raylib and Clay

--- zt-client/main.cpp
+++ zt-client/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-#include <unistd.h> // For geteuid
+#include "include/gui.hpp"
 
+#if defined(__linux__) || defined(__APPLE__)
+#include <unistd.h> // For geteuid
+#endif
 
 int main(int argc, char* argv[]) {
-    // check for sudo permissions
+#if defined(__linux__) || defined(__APPLE__)
+    // check for sudo permissions on Linux/Mac
     if(geteuid() != 0){
-        std::cout << "This app needs superuser permissions to run properly.\n"; 
+        std::cout << "Warning: This app usually needs superuser permissions to list block devices.\n"; 
+        std::cout << "Running in GUI mode, but device list might be empty.\n";
     }
+#endif
+
+    runGui();
 
     return 0;
 }
